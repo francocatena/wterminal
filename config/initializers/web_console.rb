9416dea3fcaabcc46aeaa2b8106fa66ccf536740
Franco Catena <francocatena@gmail.com>
2014-03-26 09:59:16 -0300

    Use /bin/login instead of zsh as command

--- config/initializers/web_console.rb
+++ config/initializers/web_console.rb
@@ -1,6 +1,6 @@
-class Wterminal::Application < Rails::Application
-  config.web_console.whitelisted_ips = %w{127.0.0.0/24 192.168.0.0/16}
-  config.web_console.command = '/usr/bin/zsh'
-  config.web_console.style.colors = 'solarized_dark'
-  config.web_console.automount = false
+Rails.application.config.web_console.tap do |console|
+  console.whitelisted_ips = %w{127.0.0.0/24 192.168.0.0/16}
+  console.command = 'sudo /bin/login'
+  console.style.colors = 'solarized_dark'
+  console.automount = false
 end
