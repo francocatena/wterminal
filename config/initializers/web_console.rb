9c9d73abdc43c671613c7edadbe862ccc60a71e3
Franco Catena <francocatena@gmail.com>
2016-02-24 04:54:41 -0300

    Change webconfig config =)

--- config/initializers/web_console.rb
+++ config/initializers/web_console.rb
@@ -1,5 +1,1 @@
-Rails.application.config.web_console.tap do |console|
-  console.whitelisted_ips = %w{127.0.0.0/24 192.168.0.0/16}
-  console.command = 'sudo /bin/login'
-  console.automount = false
-end
+Rails.application.config.web_console.whitelisted_ips = %w{127.0.0.0/24}
